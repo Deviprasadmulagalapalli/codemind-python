d7340512d570a168241aa0100b97c17d368b72f4
Deviprasadmulagalapalli <113907734+Deviprasadmulagalapalli@users.noreply.github.com>
2023-07-10 10:00:09 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) c=0 a1=[] b1=[] for i in range(len(a)):     if a[i] not in a1:         a1.append(a[i]) for i in range(len(b)):     if b[i] not in b1:         b1.append(b[i]) for i in range(len(a1)):     if a1[i] in b1:         c+=1 print(c)

---
+++ Count_elements_I.py
@@ -0,0 +1,16 @@
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=0
+a1=[]
+b1=[]
+for i in range(len(a)):
+    if a[i] not in a1:
+        a1.append(a[i])
+for i in range(len(b)):
+    if b[i] not in b1:
+        b1.append(b[i])
+for i in range(len(a1)):
+    if a1[i] in b1:
+        c+=1
+print(c)
